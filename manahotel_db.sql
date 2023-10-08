9f37c1d596f38e5954cc800892c727b68c4b21a6
huanviptq <huanviptq46@gmail.com>
2023-10-08 21:56:35 +0700

    Update Conduct Inventory, update DBScript

--- manahotel_db.sql
+++ manahotel_db.sql
@@ -111,7 +111,7 @@ CREATE TABLE `goods` (
 
 LOCK TABLES `goods` WRITE;
 /*!40000 ALTER TABLE `goods` DISABLE KEYS */;
-INSERT INTO `goods` VALUES ('SP000001','Sữa chua','LH000002',1,28000,30000,'Hộp',100,10,1000,'Hàng hiếm','Cực múp',NULL,NULL,'2023-10-05 17:51:06','2023-10-05 18:10:13');
+INSERT INTO `goods` VALUES ('SP000001','Sữa chua','LH000002',1,28000,30000,'Hộp',100,10,1000,'Hàng hiếm','Cực múp',NULL,NULL,'2023-10-05 17:51:06','2023-10-05 18:10:13'),('SP000002','Sprite','LH000001',1,27000,30000,'Hộp',100015,0,100000,NULL,NULL,NULL,NULL,'2023-10-07 08:43:26',NULL),('SP000003','Thịt bò khô','LH000001',1,30000,80000,'Gói',100005,0,100000,NULL,NULL,NULL,NULL,'2023-10-07 09:15:30',NULL);
 /*!40000 ALTER TABLE `goods` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -142,6 +142,68 @@ LOCK TABLES `goods_category` WRITE;
 /*!40000 ALTER TABLE `goods_category` DISABLE KEYS */;
 INSERT INTO `goods_category` VALUES ('LH000001','Hàng hóa',1,NULL,NULL,'2023-10-05 16:19:51','2023-10-05 17:08:02'),('LH000002','Dịch vụ',1,NULL,NULL,'2023-10-05 16:31:48',NULL);
 /*!40000 ALTER TABLE `goods_category` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `inventory_check`
+--
+
+DROP TABLE IF EXISTS `inventory_check`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `inventory_check` (
+                                   `inventory_check_id` varchar(50) NOT NULL,
+                                   `time_balance` timestamp NULL DEFAULT NULL,
+                                   `note` varchar(350) DEFAULT NULL,
+                                   `status` bigint DEFAULT NULL,
+                                   `created_date` timestamp NULL DEFAULT NULL,
+                                   `created_by_id` bigint DEFAULT NULL,
+                                   PRIMARY KEY (`inventory_check_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `inventory_check`
+--
+
+LOCK TABLES `inventory_check` WRITE;
+/*!40000 ALTER TABLE `inventory_check` DISABLE KEYS */;
+INSERT INTO `inventory_check` VALUES ('KK000001','2023-10-08 11:41:56','',5,'2023-10-08 11:41:56',NULL),('KK000002','2023-10-07 18:42:54',NULL,5,'2023-10-07 18:42:54',NULL);
+/*!40000 ALTER TABLE `inventory_check` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `inventory_check_detail`
+--
+
+DROP TABLE IF EXISTS `inventory_check_detail`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `inventory_check_detail` (
+                                          `inventory_check_detail_id` bigint NOT NULL AUTO_INCREMENT,
+                                          `inventory_check_id` varchar(50) NOT NULL,
+                                          `goods_id` varchar(45) NOT NULL,
+                                          `actual_inventory` bigint DEFAULT NULL,
+                                          `quantity_discrepancy` bigint DEFAULT NULL,
+                                          `value_discrepancy` float DEFAULT NULL,
+                                          `inventory` bigint DEFAULT NULL,
+                                          `cost` float DEFAULT NULL,
+                                          PRIMARY KEY (`inventory_check_detail_id`),
+                                          KEY `pk_icd_g_idx` (`goods_id`),
+                                          KEY `pk_icd_ic_idx` (`inventory_check_id`),
+                                          CONSTRAINT `pk_icd_g` FOREIGN KEY (`goods_id`) REFERENCES `goods` (`goods_id`),
+                                          CONSTRAINT `pk_icd_ic` FOREIGN KEY (`inventory_check_id`) REFERENCES `inventory_check` (`inventory_check_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=64 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `inventory_check_detail`
+--
+
+LOCK TABLES `inventory_check_detail` WRITE;
+/*!40000 ALTER TABLE `inventory_check_detail` DISABLE KEYS */;
+INSERT INTO `inventory_check_detail` VALUES (5,'KK000002','SP000001',100000,99900,2797200000,100,28000),(62,'KK000001','SP000002',100014,-1,-27000,100015,27000),(63,'KK000001','SP000003',130000,29995,899850000,100005,30000);
+/*!40000 ALTER TABLE `inventory_check_detail` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -183,7 +245,7 @@ CREATE TABLE `room` (
                         `room_id` varchar(50) NOT NULL,
                         `room_name` varchar(250) DEFAULT NULL,
                         `room_category_id` varchar(50) NOT NULL,
-                        `floor_id` int DEFAULT NULL,
+                        `floor_id` int NOT NULL,
                         `status` int DEFAULT NULL,
                         `booking_status` int DEFAULT NULL,
                         `condition_status` int DEFAULT NULL,
@@ -292,4 +354,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2023-10-06 21:24:20
+-- Dump completed on 2023-10-08 21:55:44
--- src/main/java/com/manahotel/be/service/InventoryCheckService.java
+++ src/main/java/com/manahotel/be/service/InventoryCheckService.java
@@ -2,6 +2,7 @@ package com.manahotel.be.service;
 
 import com.manahotel.be.common.constant.Status;
 import com.manahotel.be.common.util.IdGenerator;
+import com.manahotel.be.exception.EmptyListException;
 import com.manahotel.be.exception.ResourceNotFoundException;
 import com.manahotel.be.model.dto.InventoryCheckDTO;
 import com.manahotel.be.model.dto.InventoryCheckDetailDTO;
@@ -11,7 +12,7 @@ import com.manahotel.be.model.entity.InventoryCheckDetail;
 import com.manahotel.be.repository.GoodsRepository;
 import com.manahotel.be.repository.InventoryCheckDetailRepository;
 import com.manahotel.be.repository.InventoryCheckRepository;
-import jakarta.persistence.Tuple;
+import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -24,6 +25,7 @@ import java.util.Optional;
 public class InventoryCheckService {
 
     private static final Long TEMPORARY = Status.TEMPORARY.getStatusId();
+    private static final Long CANCEL = Status.CANCEL.getStatusId();
 
     @Autowired
     private InventoryCheckRepository repository;
@@ -38,36 +40,30 @@ public class InventoryCheckService {
         return repository.findAll();
     }
 
-    public String createInventoryCheck(InventoryCheckDTO dto, List<InventoryCheckDetailDTO> listDetailDTO) {
-
-        InventoryCheck latestCheck = repository.findTopByOrderByInventoryCheckIdDesc();
-        String latestId = (latestCheck == null) ? null : latestCheck.getInventoryCheckId();
-        String nextId = IdGenerator.generateId(latestId, "KK");
-
-        InventoryCheck check = new InventoryCheck();
-        check.setInventoryCheckId(nextId);
+    private void commonMapping(InventoryCheck check, InventoryCheckDTO dto, List<InventoryCheckDetailDTO> listDetailDTO) {
         check.setStatus(dto.getStatus());
-        if(check.getStatus().equals(TEMPORARY)) {
+
+        if (check.getStatus().equals(TEMPORARY)) {
             check.setTimeBalance(null);
-        }
-        else {
+        } else {
             Timestamp timeBalance = Optional.ofNullable(dto.getTimeBalance())
                     .orElseGet(() -> new Timestamp(System.currentTimeMillis()));
             check.setTimeBalance(timeBalance);
         }
 
         check.setNote(dto.getNote());
-        check.setCreatedDate(new Timestamp(System.currentTimeMillis()));
 
         repository.save(check);
 
+        repository2.deleteInventoryCheckDetailByInventoryCheckId(check.getInventoryCheckId());
+
         List<InventoryCheckDetail> list = new ArrayList<>();
 
-        for(InventoryCheckDetailDTO detailDTO : listDetailDTO) {
+        for (InventoryCheckDetailDTO detailDTO : listDetailDTO) {
 
             InventoryCheckDetail detail = new InventoryCheckDetail();
 
-            InventoryCheck ic = findInventoryCheckById(nextId);
+            InventoryCheck ic = findInventoryCheckById(check.getInventoryCheckId());
             detail.setInventoryCheck(ic);
 
             Goods goods = findGoods(detailDTO.getGoodsId());
@@ -82,15 +78,55 @@ public class InventoryCheckService {
             list.add(detail);
         }
 
-        repository2.saveAll(list);
+        if(list.isEmpty()) {
+            repository2.saveAll(list);
+        }
+        else{
+            throw new EmptyListException("Chi tiết kiểm kho không được để trống");
+        }
+    }
+
+    public String createInventoryCheck(InventoryCheckDTO dto, List<InventoryCheckDetailDTO> listDetailDTO) {
+
+        InventoryCheck latestCheck = repository.findTopByOrderByInventoryCheckIdDesc();
+        String latestId = (latestCheck == null) ? null : latestCheck.getInventoryCheckId();
+        String nextId = IdGenerator.generateId(latestId, "KK");
+
+        InventoryCheck check = new InventoryCheck();
+        check.setInventoryCheckId(nextId);
+        check.setCreatedDate(new Timestamp(System.currentTimeMillis()));
+
+        commonMapping(check, dto, listDetailDTO);
 
         return "Tạo kiểm kho thành công";
     }
 
     public String updateInventoryCheck(String id, InventoryCheckDTO dto, List<InventoryCheckDetailDTO> listDetailDTO) {
+        InventoryCheck check = findInventoryCheckById(id);
+
+        if (check == null) {
+            return null;
+        }
+
+        commonMapping(check, dto, listDetailDTO);
+
         return "Cập nhật kiểm kho thành công";
     }
 
+    public List<InventoryCheck> getAllCheck() {
+        return repository.findAll();
+    }
+
+    public String cancelCheck(String id) {
+        InventoryCheck check = findInventoryCheckById(id);
+        check.setStatus(CANCEL);
+        repository.save(check);
+        return "Hủy kiểm kho thành công";
+    }
+
+    public List<InventoryCheckDetail> findListInvetoryCheckDetailByInventoryCheckId(String id) {
+        return repository2.findListInventoryCheckDetailByInventoryCheckId(id);
+    }
 
     public InventoryCheck findInventoryCheckById(String id) {
         return repository.findById(id)
@@ -103,6 +139,6 @@ public class InventoryCheckService {
     }
 
     public InventoryCheckResponse getInventoryCheckSummary(String id) {
-         return repository2.getInventoryCheckSummary(id);
+        return repository2.getInventoryCheckSummary(id);
     }
 }
