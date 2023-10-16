f09ca155f5618088ccc47aa0f70bd3285f2fa23b
khanhtb1306 <trinhbaokhanh1306@gmail.com>
2023-10-17 00:46:34 +0700

    update unittest room,roomClass

---
+++ src/test/java/com/manahotel/be/service/RoomClassServiceTest.java
@@ -0,0 +1,42 @@
+package com.manahotel.be.service;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RoomClassServiceTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void getAllRoomClassWithRoomCount() {
+    }
+
+    @Test
+    void createRoomClass() {
+    }
+
+    @Test
+    void updateRoomClass() {
+    }
+
+    @Test
+    void deleteRoomClassById() {
+    }
+
+    @Test
+    void getRoomCategoryById() {
+    }
+
+    @Test
+    void getAllRoomClassWithListRoom() {
+    }
+}
---
+++ src/test/java/com/manahotel/be/service/RoomServiceTest.java
@@ -0,0 +1,58 @@
+package com.manahotel.be.service;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RoomServiceTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void getAllRoom() {
+    }
+
+    @Test
+    void createRoom() {
+    }
+
+    @Test
+    void updateRoom() {
+    }
+
+    @Test
+    void deleteRoomById() {
+    }
+
+    @Test
+    void getRoomById() {
+    }
+
+    @Test
+    void getFloorById() {
+    }
+
+    @Test
+    void getAllFloor() {
+    }
+
+    @Test
+    void createFloor() {
+    }
+
+    @Test
+    void updateFloor() {
+    }
+
+    @Test
+    void deleteFloorById() {
+    }
+}
