ae96712dbd006e0547b67a6ff48fe18f391e7c15
khanhtb1306 <trinhbaokhanh1306@gmail.com>
2023-10-20 01:58:34 +0700

    swagger config

--- src/main/java/com/manahotel/be/config/SwaggerConfig.java
+++ src/main/java/com/manahotel/be/config/SwaggerConfig.java
@@ -1,26 +1,29 @@
 package com.manahotel.be.config;
 
+
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
-import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+
 @Configuration
 public class SwaggerConfig {
+    final String securitySchemeName = "bearerAuth";
 
     @Bean
     public OpenAPI customOpenAPI() {
         return new OpenAPI()
-                .info(new Info().title("ManaHotel")
-                        .version("1.0")
-                        .description("Your API Description"))
-                .components(new io.swagger.v3.oas.models.Components()
-                        .addSecuritySchemes("bearer-key", new SecurityScheme()
+                .addSecurityItem(new SecurityRequirement().addList(securitySchemeName))
+                .components(new Components()
+                        .addSecuritySchemes(securitySchemeName, new SecurityScheme()
+                                .name(securitySchemeName)
                                 .type(SecurityScheme.Type.HTTP)
                                 .scheme("bearer")
-                                .bearerFormat("JWT")
-                        )
-                );
+                                .bearerFormat("JWT")));
+
+
     }
 }
