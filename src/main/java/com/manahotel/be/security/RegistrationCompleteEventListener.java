6917d150f1f3b236f95deb75e32ca1d64dad65c1
khanhtb1306 <trinhbaokhanh1306@gmail.com>
2023-10-24 17:30:04 +0700

    update sendEmail

--- src/main/java/com/manahotel/be/security/RegistrationCompleteEventListener.java
+++ src/main/java/com/manahotel/be/security/RegistrationCompleteEventListener.java
@@ -54,13 +54,13 @@ public class RegistrationCompleteEventListener implements ApplicationListener<Re
     }
 
     public void sendPasswordResetVerificationEmail(String url, Staff staff) throws UnsupportedEncodingException, MessagingException, jakarta.mail.MessagingException {
-        String subject = "Password Reset Request Verification";
-        String senderName = "User Registration Portal Service";
-        String mailContent = "<p> Hi, " + staff.getStaffName() + ", </p>" +
-                "<p><b>You recently requested to reset your password,</b>" + "" +
-                "Please, follow the link below to complete the action.</p>" +
-                "<a href=\"" + url + "\">Reset password</a>" +
-                "<p> Users Registration Portal Service";
+        String subject = "Xác minh yêu cầu đặt lại mật khẩu";
+        String senderName = "Dịch vụ người dùng";
+        String mailContent = "<p> Chào, " + staff.getStaffName() + ", </p>" +
+                "<p><b>Gần đây bạn đã yêu cầu đặt lại mật khẩu của mình,</b>" + "" +
+                "Vui lòng nhấp vào liên kết bên dưới để hoàn thành hành động.</p>" +
+                "<a href=\"" + url + "\">Đặt lại mật khẩu</a>" +
+                "<p> Dịch vụ cổng đăng ký người dùng";
         jakarta.mail.internet.MimeMessage message = mailSender.createMimeMessage();
         MimeMessageHelper messageHelper = new MimeMessageHelper(message, true);
         messageHelper.setFrom("nguyenhuync2937@gmail.com", senderName);
