12849bd423e908278d41fee3f3fe00b0b3d4dccf
huanviptq <huanviptq46@gmail.com>
2023-11-06 17:55:19 +0700

    fix reservation system

--- src/main/java/com/manahotel/be/repository/ReservationRepository.java
+++ src/main/java/com/manahotel/be/repository/ReservationRepository.java
@@ -1,21 +1,16 @@
 package com.manahotel.be.repository;
 
 import com.manahotel.be.model.entity.Reservation;
-import com.manahotel.be.model.entity.Room;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
-import java.sql.Timestamp;
-import java.util.Date;
 import java.util.List;
 
 @Repository
 public interface ReservationRepository extends JpaRepository<Reservation, String> {
+
     @Query("SELECT r FROM Reservation r " +
             "LEFT JOIN ReservationDetail rd ON r.reservationId = rd.reservation.reservationId")
     List<Object[]> findReservationsWithRooms();
-
-    Reservation findTopByOrderByReservationIdDesc();
 }
