bd210abfa8a994bf99ad0773e595e72967b9b6ee
khanhtb1306 <trinhbaokhanh1306@gmail.com>
2023-11-13 00:47:28 +0700

    update policy

--- src/main/java/com/manahotel/be/service/PolicyService.java
+++ src/main/java/com/manahotel/be/service/PolicyService.java
@@ -76,7 +76,7 @@ public class PolicyService {
             Policy policy = policyRepository.getPolicyByPolicyName(PolicyName);
             List<PolicyDetail> policyDetail = policyDetailRepository.getPolicyDetailByPolicyIdNotStatus6(policy.getPolicyId());
             Policy.put("Policy", policy);
-            Policy.put("List" + PolicyName + "Detail", policyDetail);
+            Policy.put("LIST_" + PolicyName + "_DETAIL", policyDetail);
             log.info("----- Get All Policy Detail By " + PolicyName + " End ------");
             return ResponseUtils.success(Policy,"IsSuccess");
         }catch (Exception e){
