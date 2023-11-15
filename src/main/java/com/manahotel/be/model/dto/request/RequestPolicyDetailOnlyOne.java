dbe4655c3dcf43831ff1cf425c95fe3a2d81e7e2
khanhtb1306 <trinhbaokhanh1306@gmail.com>
2023-11-15 16:41:21 +0700

    Update policyDetail only

---
+++ src/main/java/com/manahotel/be/model/dto/request/RequestPolicyDetailOnlyOne.java
@@ -0,0 +1,10 @@
+package com.manahotel.be.model.dto.request;
+
+import com.manahotel.be.model.dto.PolicyDetailDTO;
+import lombok.Data;
+
+@Data
+public class RequestPolicyDetailOnlyOne {
+    private PolicyDetailDTO policyDetailDTO;
+
+}
