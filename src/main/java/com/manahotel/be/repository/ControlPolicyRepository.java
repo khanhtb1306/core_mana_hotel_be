758d66b6593edfec7ad08013ea4b05ff9dc0a47f
khanhtb1306 <trinhbaokhanh1306@gmail.com>
2023-12-01 01:44:25 +0700

    fix add, update ControlPolicy

--- src/main/java/com/manahotel/be/repository/ControlPolicyRepository.java
+++ src/main/java/com/manahotel/be/repository/ControlPolicyRepository.java
@@ -1,9 +1,9 @@
 package com.manahotel.be.repository;
 
 import com.manahotel.be.model.entity.ControlPolicy;
-import com.manahotel.be.model.entity.Policy;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 @Repository
 public interface ControlPolicyRepository extends JpaRepository<ControlPolicy, Long> {
+    ControlPolicy findControlPolicyByReservationDetail_ReservationDetailId(Long id);
 }
