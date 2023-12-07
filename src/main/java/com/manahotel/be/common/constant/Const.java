dedf78a7fba8c2878657280febe0763f1f6ed928
khanhtb1306 <trinhbaokhanh1306@gmail.com>
2023-12-08 01:30:56 +0700

    update

--- src/main/java/com/manahotel/be/common/constant/Const.java
+++ src/main/java/com/manahotel/be/common/constant/Const.java
@@ -4,7 +4,7 @@ import java.util.regex.Pattern;
 
 public class Const {
     public static final String INVOICE_ID = "HD000000";
-    public static final String ORDER_ID = "HD000000";
+    public static final String ORDER_ID = "DH000000";
     public static final String RESERVATION_ID = "DP000000";
     public static final String CUSTOMER_ID = "C000000";
 
